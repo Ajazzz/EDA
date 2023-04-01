aef75fbb03eb4a90efb48fe830c06336c4a431a2
Ajaz <41190679+Ajazzz@users.noreply.github.com>
2023-04-01 22:21:39 +0530

    Update basic.py

--- Basic EDA/basic.py
+++ Basic EDA/basic.py
@@ -1,1 +1,9 @@
+# Exploratory Data Analysis (EDA) is an essential step in any data analysis project, 
+# as it helps to understand the data and its characteristics. In Python, there are various libraries that you can use for EDA, 
+# including Pandas, Matplotlib, and Seaborn. Here's a basic EDA workflow in Python:
 
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv('data.csv')
