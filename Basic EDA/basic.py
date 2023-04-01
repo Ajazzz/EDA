5846264374d80c134869525218a4b9db8bd7ef62
Ajaz <41190679+Ajazzz@users.noreply.github.com>
2023-04-01 22:23:59 +0530

    Update basic.py

--- Basic EDA/basic.py
+++ Basic EDA/basic.py
@@ -6,4 +6,33 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
+# Load data: Load the data into a Pandas dataframe using the read_csv function.
 df = pd.read_csv('data.csv')
+
+# Get an overview of the data: Use the head() and tail() functions to view the first and last rows of the dataframe, respectively. 
+#Also, use the info() function to get information about the data types of each column and the describe() function to get basic statistical information about the dataframe.
+
+print(df.head())
+print(df.tail())
+print(df.info())
+print(df.describe())
+
+# Handling missing values: Check if there are any missing values in the dataset using the isnull() and sum() functions.
+
+print(df.isnull().sum())
+
+# Visualizing data: Use visualizations to understand the data. 
+# You can use Matplotlib or Seaborn to create various types of plots, such as histograms, scatter plots, box plots, and bar plots
+
+# Histogram
+sns.histplot(data=df, x='column_name')
+
+# Scatter plot
+sns.scatterplot(data=df, x='column_name_1', y='column_name_2')
+
+# Box plot
+sns.boxplot(data=df, x='column_name')
+
+# Bar plot
+sns.countplot(data=df, x='column_name')
