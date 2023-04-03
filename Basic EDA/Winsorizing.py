7f917f2f9900e4e0687d6c25889b8e4f78bdedc8
Ajaz <41190679+Ajazzz@users.noreply.github.com>
2023-04-03 21:52:06 +0530

    Update Winsorizing.py

--- Basic EDA/Winsorizing.py
+++ Basic EDA/Winsorizing.py
@@ -1,1 +1,13 @@
 
+import numpy as np
+
+def winsorize(arr, limits):
+    '''
+    Winsorize the values in an array.
+    arr: numpy array
+    limits: tuple, representing the lower and upper percentile limits
+    '''
+    lower_limit, upper_limit = np.percentile(arr, limits)
+    arr[arr < lower_limit] = lower_limit
+    arr[arr > upper_limit] = upper_limit
+    return arr
