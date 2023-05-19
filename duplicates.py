a29b79c9ccc95ebbfe639c2675b74a0ede77dea7
Ajaz <41190679+Ajazzz@users.noreply.github.com>
2023-05-19 07:03:48 +0530

    Update duplicates.py

--- duplicates.py
+++ duplicates.py
@@ -1,5 +1,5 @@
 
-# To find duplicates in a Pandas DataFrame in Python, you can use the duplicated() method, 
+'''To find duplicates in a Pandas DataFrame in Python, you can use the duplicated() method, 
 # which returns a boolean Series indicating whether each row is a duplicate or not.
 # Here's an example code snippet to find duplicates in a DataFrame using Python:
 
